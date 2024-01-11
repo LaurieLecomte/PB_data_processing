6deb4923bb4da1a88d9f5d1d960fbf79a186c942
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2024-01-11 15:57:31 -0500

    updated tests for pbmm2 and longQC

--- 01_scripts/QC_longQC.sh
+++ 01_scripts/QC_longQC.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 
-# Summary plots for CCS PacBio data using longQC
+# Summary plots for PacBio data using longQC
 # parallel -a 02_infos/ind_PB.txt -j 4 /project/lbernatchez/users/lalec31/projets_labo/FISHES/02_long_reads/PB_data_processing/PB_data_processing/01_scripts/
 
 # head -n2 02_infos/ind_PB.txt | srun -p ibis_small -c 2 -J "{}"_QC_longQC --mem=10G -o log/QC_longQC_"{}"_%j.log /bin/sh 01_scripts/QC_longQC.sh "{}" &
 
 # Raw data path from valeria : /mnt/ibis/lbernatchez/00_raw_data/2021-09-20_anne-laure_long_reads_coregone_safo_pacbio/
 
-# srun -p small -c 4 -J "{}"_QC_longQC --mem=10G -o log/QC_longQC_"{}"_%j.log /bin/sh 01_scripts/QC_longQC.sh "safoUMIx-19-20" &
+# srun -p medium -c 8 --mem=100G --time=7-00:00:00 -J "safoSABs_027-21"_QC_longQC -o log/QC_longQC_"safoSABs_027-21"_%j.log /bin/sh 01_scripts/QC_longQC.sh "safoSABs_027-21" &
 
 # VARIABLES
 SAMPLE=$1
@@ -15,12 +15,13 @@ RAW_DATA_DIR="04_raw_data"
 CCS_BAM="$RAW_DATA_DIR/"$SAMPLE".ccs.bam" 
 SUBREAD_BAM="$RAW_DATA_DIR/"$SAMPLE".subreads.bam" 
 OUT_DIR="QC"
-CPU=4
+CPU=8
 
-LONGQC_EXEC="/project/lbernatchez/users/lalec31/.conda/pkgs/longqc-1.2.0c-hdfd78af_0/bin/longQC.py"
+#LONGQC_EXEC="/project/lbernatchez/users/lalec31/.conda/pkgs/longqc-1.2.0c-hdfd78af_0/bin/longQC.py"
+LONGQC_EXEC="/project/lbernatchez/users/lalec31/softwares/LongQC/longQC.py"
 
 # LOAD REQUIRED MODULES
-module load python/3.7
+#module load python/3.9
 
 # Create output directory
 #if [[ ! -d "$OUT_DIR"/longQC/"$SAMPLE" ]]
@@ -36,4 +37,12 @@ module load python/3.7
 #python $LONGQC_EXEC sampleqc -x pb-sequel -s $SAMPLE $CCS_BAM -o QC/longQC/$SAMPLE -p $CPU --trim_output QC/longQC/$SAMPLE/trimmed
 
 # Run longQC on subreads bam
-python $LONGQC_EXEC sampleqc -x pb-sequel -s $SAMPLE $SUBREAD_BAM -o QC/longQC/$SAMPLE -p $CPU --trim_output QC/longQC/$SAMPLE/trimmed_subreads
+# First subset to quicken process
+module load samtools/1.15
+samtools index $SUBREAD_BAM -@ $CPU
+samtools view $SUBREAD_BAM -b -s 0.05 --threads $CPU > $RAW_DATA_DIR/"$SAMPLE".subreads.subs0.05.bam 
+
+python $LONGQC_EXEC sampleqc -x pb-sequel -s $SAMPLE $RAW_DATA_DIR/"$SAMPLE".subreads.subs0.05.bam -o QC/longQC/$SAMPLE -p $CPU --trim_output QC/longQC/$SAMPLE/"$SAMPLE".subreads.subs0.05.trimmed.fastq -d
+
+#python $LONGQC_EXEC sampleqc -x pb-sequel -s $SAMPLE $SUBREAD_BAM -o QC/longQC/$SAMPLE -p $CPU --trim_output QC/longQC/$SAMPLE/"$SAMPLE".subreads.trimmed.fastq -d
+#bgzip QC/longQC/$SAMPLE/"$SAMPLE".subreads.trimmed.fastq
--- 01_scripts/test_pbmm2.sh
+++ 01_scripts/test_pbmm2.sh
@@ -3,7 +3,7 @@
 # Test pbmm2 with PacBio long reads
 #
 # valeria
-# srun -p ibis_large -c 6 --time=21-00:00 -J test_pbmm2 --mem=70G -o log/test_pbmm2_%j.log /bin/sh 01_scripts/test_pbmm2.sh "safoCHIm_2359-21" &
+# srun -p medium -c 8 --time=7-00:00 -J safoPUVx_001-21_test_pbmm2 --mem=100G -o log/safoPUVx_001-21_test_pbmm2_%j.log /bin/sh 01_scripts/test_pbmm2.sh "safoPUVx_001-21" &
 
 # VARIABLES
 SAMPLE=$1
@@ -15,7 +15,7 @@ SUBREADS_BAM="$RAW_DATA_DIR/"$SAMPLE".subreads.bam"
 
 ALIGNED_DIR="06_aligned"
 
-CPU=6
+CPU=8
 
 # 0. Create output dir
 if [[ ! -d "$ALIGNED_DIR"/pbmm2 ]]
@@ -27,7 +27,13 @@ fi
 #pbmm2 index $GENOME 03_genome/genome.fasta.mmi
 
 # 1. Run pbmm2, sort and indem bam output
-pbmm2 align $GENOME $CCS_BAM $ALIGNED_DIR/pbmm2/"$SAMPLE".ccs.bam --sort --sample $SAMPLE --bam-index BAI --preset CCS -j $(($CPU / 2)) -J $(($CPU / 2)) --log-level TRACE
+#pbmm2 align $GENOME $CCS_BAM $ALIGNED_DIR/pbmm2/"$SAMPLE".ccs.bam --sort --sample $SAMPLE --bam-index BAI --preset CCS -j $(($CPU / 2)) -J $(($CPU / 2)) --log-level TRACE
 
+
+
+# try with subreads
 pbmm2 align $GENOME $SUBREADS_BAM $ALIGNED_DIR/pbmm2/"$SAMPLE".subreads.bam --sort --sample $SAMPLE --bam-index BAI --preset SUBREAD -j $(($CPU / 2)) -J $(($CPU / 2)) --log-level TRACE
+
+# try with trimmed subreads 
+pbmm2 align $GENOME QC/longQC/$SAMPLE/"$SAMPLE".subreads.trimmed.fastq $ALIGNED_DIR/pbmm2/"$SAMPLE".subreads.trimmed.bam --sort --sample $SAMPLE --bam-index BAI --preset SUBREAD -j $(($CPU / 2)) -J $(($CPU / 2)) --log-level TRACE
 
