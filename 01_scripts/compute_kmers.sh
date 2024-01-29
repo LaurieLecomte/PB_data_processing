04874e2bcf2ca1cd835da0a856259d48dff51e30
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2024-01-29 18:07:20 -0500

    added main scripts

---
+++ 01_scripts/compute_kmers.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+# Pre-computing high frequency k-mers (e.g., top 0.02% most frequent) in a reference with meryl
+# Launch in a conda env where Winnowmap has been installed, or load required modules/wheels
+
+# manitou
+# srun -p medium -c 4 --time=7-00:00 -J compute_kmers --mem=70G -o log/compute_kmers_%j.log 01_scripts/compute_kmers.sh &
+
+# valeria
+# srun -p ibis_medium -c 4 --time=7-00:00 -J compute_kmers --mem=70G -o log/compute_kmers_%j.log 01_scripts/compute_kmers.sh &
+
+# VARIABLES
+SAMPLE=$1
+GENOME="03_genome/genome.fasta"
+ALIGNED_DIR="06_aligned"
+
+CPU=6
+
+# 0. Create output dir
+if [[ ! -d "$ALIGNED_DIR" ]]
+then
+  mkdir $ALIGNED_DIR
+fi
+
+# 1. Pre-computing high frequency k-mers (e.g., top 0.02% most frequent) in a reference with meryl
+meryl count k=15 output $ALIGNED_DIR/merylDB $GENOME threads=$CPU
+meryl print greater-than distinct=0.9998 $ALIGNED_DIR/merylDB > $ALIGNED_DIR/repetitive_k15.txt
---
+++ 01_scripts/winnowmap.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+
+# Align ONT reads with winnowmap
+# Launch in a conda env where winnowmap and smrtlink-tools (tested with version 10.1.0) have been installed, or load required modules/wheels
+
+# manitou
+# parallel -a 02_infos/ind_ONT.txt -j 4 srun -p medium -c 4 --time=7-00:00 -J winnowmap_{} --mem=70G -o log/winnowmap_{}_%j.log /bin/sh 01_scripts/winnowmap.sh {} &
+
+# valeria
+# parallel -a 02_infos/ind_ONT.txt -j 4 srun -p ibis_medium -c 6 --time=7-00:00 -J winnowmap_{} --mem=70G -o log/winnowmap_{}_%j.log /bin/sh 01_scripts/winnowmap.sh {} &
+
+# VARIABLES
+SAMPLE=$1
+GENOME="03_genome/genome.fasta"
+RAW_DATA_DIR="04_raw_data"
+FILT_DIR="05_filtered"
+CCS_BAM="$RAW_DATA_DIR/"$SAMPLE".ccs.bam" 
+SUBREADS_BAM="$RAW_DATA_DIR/"$SAMPLE".subreads.bam" 
+
+ALIGNED_DIR="06_aligned"
+
+CPU=4
+
+# LOAD REQUIRED MODULES
+module load samtools/1.15
+
+# 0. Create output dir
+if [[ ! -d "$ALIGNED_DIR"/winnowmap ]]
+then
+  mkdir $ALIGNED_DIR/winnowmap
+fi
+
+# O. Convert .ccs bam file to fasta
+#pbindex $CCS_BAM
+#bam2fasta -o $RAW_DATA_DIR/"$SAMPLE".ccs $CCS_BAM -p "$SAMPLE"
+
+
+# 1. Run winnowmap on converted file, using pre-computed kmers
+## -Y enables mapping of soft-clipped reads 
+winnowmap -t $CPU --MD --cs -W $ALIGNED_DIR/winnowmap/repetitive_k15.txt -a -x map-pb -Y -R "@RG\tID:$SAMPLE\tSM:$SAMPLE" $GENOME $RAW_DATA_DIR/"$SAMPLE".ccs.fasta.gz > $ALIGNED_DIR/winnowmap/"$SAMPLE".ccs.sam
+
+# Convert to bam, sort and index 
+samtools sort --threads $CPU  $ALIGNED_DIR/winnowmap/"$SAMPLE".ccs.sam -o $ALIGNED_DIR/winnowmap/"$SAMPLE".ccs.bam
+samtools index $ALIGNED_DIR/winnowmap/"$SAMPLE".ccs.bam
+#samtools view -b $ALIGNED_DIR/winnowmap/"$SAMPLE".ccs.sam --threads $CPU | samtools sort --threads $CPU --write-index > $ALIGNED_DIR/winnowmap/"$SAMPLE".ccs.bam
+
+# Clean up
+#rm $ALIGNED_DIR/winnowmap/"$SAMPLE".ccs.sam
